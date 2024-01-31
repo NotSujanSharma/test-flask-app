3bb7b4e2e750e43345394bc55aa29f5becb4bf79
notsujansharma <sujan@brokenai.ca>
2024-01-30 20:36:27 -0500

    testing with flask

--- main.py
+++ main.py
@@ -9,14 +9,25 @@ def error():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    
     if request.method == 'POST':
         url = request.form['url']
-        try:
-            response = requests.get(url)
-            return render_template('result.html', response=response.text)
-        except requests.RequestException as e:
-            return render_template('result.html', response=f"Error: {e}")
-    return render_template('index.html')
+        if request.form['method']=='GET':
+            try:
+                response = requests.get(url)
+                return render_template('result.html', response=response.text, title='Result')
+            except requests.RequestException as e:
+                return render_template('result.html', response=f"Error: {e}", title='Result')
+        elif request.form['method']=='POST':
+            url = request.form['url']
+            data = request.form['data']
+            try:
+                response = requests.post(url, data=data)
+                return render_template('result.html', response=response.text, title='Result')
+            except requests.RequestException as e:
+                return render_template('result.html', response=f"Error: {e}", title='Result')
+    
+    return render_template('index.html', title="Online HTTP Requester", response=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
